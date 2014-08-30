dc1dcc354557d14151bd641f2ba55524ba73fbe0
barrybbb <yang6600>
2014-08-30 21:10:44 +0800

    upload all files

---
+++ public/analytics.js
@@ -0,0 +1,2 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fec912ecc405ccd050e4cdf452ef4e85a' type='text/javascript'%3E%3C/script%3E"));
